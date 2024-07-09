1bb1b953b00f8df0389b8b4a1941db9858302216
Tornike Tsagareishvili <dht5152@gmail.com>
2024-07-09 21:22:38 +0400

    Add: Header HTML

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,18 @@
 import "./header.scss";
 
 export function Header() {
-  return <header>dsdf</header>;
+  return (
+    <header>
+      <img src="" alt="Main Logo" />
+      <ul>
+        <li>მთავარი</li>
+        <li>ჩვენს შესახებ</li>
+        <li>კონტაქტი</li>
+        <li>შესვლა</li>
+        <li>რეგისტრაცია</li>
+      </ul>
+    </header>
+  );
 }
 
 export default Header;
