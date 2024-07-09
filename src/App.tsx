022d22d2aa0cab741f9d5d667ab4303ef001688c
Tornike Tsagareishvili <dht5152@gmail.com>
2024-07-09 20:59:06 +0400

    HomePage Component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import { Suspense, lazy } from "react";
 import { Route, Routes } from "react-router-dom";
 import { PublicLayout } from "./layouts/PublicLayout";
 
+const HomePage = lazy(() => import("./pages/HomePage"));
+
 function App() {
   return (
     <>
       <Suspense fallback={<p>Loading...</p>}>
         <Routes>
-          <Route element={<PublicLayout />}></Route>
+          <Route element={<PublicLayout />}>
+            <Route path="/" element={<HomePage />}></Route>
+          </Route>
         </Routes>
       </Suspense>
     </>
