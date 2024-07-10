d9b365694ce3fa3472c1c8199d12cd055c08b3ee
Tornike Tsagareishvili <dht5152@gmail.com>
2024-07-10 22:21:32 +0400

    Add: Change Vite For Absolute Imports Bug Fix

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,44 @@
+import mainLogo from "src/assets/main-logo.png";
+import linkedinLogo from "src/assets/linkedin-logo.png";
+import instagramLogo from "src/assets/Instagram-logo.png";
+import facebookLogo from "src/assets/facebook-logo.png";
+import userIcon from "src/assets/user-icon.png";
+
 import "./header.scss";
 
 export function Header() {
   return (
-    <header>
-      <img src="" alt="Main Logo" />
-      <ul>
-        <li>მთავარი</li>
-        <li>ჩვენს შესახებ</li>
-        <li>კონტაქტი</li>
-        <li>შესვლა</li>
-        <li>რეგისტრაცია</li>
-      </ul>
-    </header>
+    <div className="header-spacing">
+      <header className="header">
+        <figure className="socials">
+          <img className="main-logo" src={mainLogo} alt="Main Logo" />
+          <img
+            className="socials-logo"
+            src={linkedinLogo}
+            alt="Linkedin Logo"
+          />
+          <img
+            className="socials-logo"
+            src={instagramLogo}
+            alt="Instagram Logo"
+          />
+          <img
+            className="socials-logo"
+            src={facebookLogo}
+            alt="Facebook Logo"
+          />
+        </figure>
+        <ul>
+          <li>მთავარი</li>
+          <li>ჩვენს შესახებ</li>
+          <li>კონტაქტი</li>
+          <figure className="sign-in">
+            <img src={userIcon} alt="User Icon" />
+            <figcaption>შესვლა</figcaption>
+          </figure>
+        </ul>
+      </header>
+    </div>
   );
 }
 
